0bb9f0730022216f94894148d0c80b39f43adf7c
SalmaEl0uahdani <s.e.o.professionaluses@gmail.com>
2023-07-14 19:31:46 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,11 +5,12 @@
  *
  *Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i;
-	char alp [52] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	for (i=0; i < 52 ; i++)
+
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 		putchar('\n');
