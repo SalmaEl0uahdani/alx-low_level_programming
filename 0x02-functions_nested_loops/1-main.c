be92897e7d04858fb9b58299ea1fed8414d234b5
SalmaEl0uahdani <s.e.o.professionaluses@gmail.com>
2023-07-18 12:14:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main -  prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
