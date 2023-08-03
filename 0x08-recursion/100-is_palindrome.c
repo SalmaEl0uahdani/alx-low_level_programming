0953406e20baa81b56b3784720007ce1d610bad3
SalmaEl0uahdani <s.e.o.professionaluses@gmail.com>
2023-08-02 17:26:27 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /** _strlen - is a common C function that is used to
-* calculate the length of a given string.
-* @s: takes a string input
-* Return: The length of the string
-*/
+ * calculate the length of a given string.
+ * @s: takes a string input
+ * Return: The length of the string
+ */
 
 int _strlen(char *s)
 {
