81da57a2015820fdb0c4de01945f0a0c0d96d7f4
SalmaEl0uahdani <s.e.o.professionaluses@gmail.com>
2023-11-02 21:50:53 +0100

    CommitMessage

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,18 +1,40 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
-/**
- * main - prints buffer in hexa
- * @argc: the address of memory to print
- * @argv: the size of the memory to print
- *
- * Return: Nothing.
- */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    int num1, num2;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Error\n");
+            return (98);
+        }
+    }
+
+    for (int i = 0; argv[2][i] != '\0'; i++)
+    {
+        if (!isdigit(argv[2][i]))
+        {
+            printf("Error\n");
+            return (98);
+        }
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    printf("%d\n", num1 * num2);
+
     return (0);
 }
+
