a957f9e99ef4367cda5c266c34c2ff6cbc7087f0
SalmaEl0uahdani <s.e.o.professionaluses@gmail.com>
2023-11-02 22:31:52 +0100

    CommitMessage

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,96 +1,126 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: no return.
  */
-int _putchar(char c)
+void _is_zero(char *argv[])
 {
-    return (write(1, &c, 1));
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
 }
 
 /**
- * print_number - prints an integer char by char
- * @n: The integer to print
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
  *
- * Return: void
+ * Return: pointer of a char array.
  */
-void print_number(int n)
+char *_initialize_array(char *ar, int lar)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	int i = 0;
 
-    if (n == 0)
-        _putchar('0');
-
-    if (n / 10)
-        print_number(n / 10);
-
-    _putchar(n % 10 + '0');
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 98 on error
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
  */
-int main(int argc, char *argv[])
+int _checknum(char *argv[], int n)
 {
-    int num1, num2;
+	int ln;
 
-    if (argc != 3)
-    {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        exit(98);
-    }
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 
-    for (int i = 0; argv[1][i] != '\0'; i++)
-    {
-        if (!isdigit(argv[1][i]))
-        {
-            _putchar('E');
-            _putchar('r');
-            _putchar('r');
-            _putchar('o');
-            _putchar('r');
-            _putchar('\n');
-            exit(98);
-        }
-    }
+	return (ln);
+}
 
-    for (int i = 0; argv[2][i] != '\0'; i++)
-    {
-        if (!isdigit(argv[2][i]))
-        {
-            _putchar('E');
-            _putchar('r');
-            _putchar('r');
-            _putchar('o');
-            _putchar('r');
-            _putchar('\n');
-            exit(98);
-        }
-    }
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char **argv)
+{
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    print_number(num1 * num2);
-    _putchar('\n');
-
-    return (0);
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
+	{
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
+	}
+	printf("%s\n", nout);
+	return (0);
 }
 
